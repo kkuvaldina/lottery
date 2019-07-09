fff170756b5c77da7d0275bc419278c03d397071
Kseniia <kuvaldina.ks@gmail.com>
2019-07-08 21:03:04 -0600

    2

--- js/script.js
+++ js/script.js
@@ -1,2 +1,25 @@
-var userInput = document.getElementById('input-num').value;
+var x = document.getElementById("userInput").value;
+var lottery = [];
+var lotto = "";
 
+/*function validateForm() {
+    x = document.getElementById("userInput").value;
+    if (x == "") {
+    alert("Name must be filled out");
+    return false;
+    }
+  }*/
+
+
+
+
+/*for (var i = 0; i < x; i++) 
+{
+    lottery[i] = Math.ceil(Math.random * 99);
+}
+
+for (var i = 0; i < x; i++) 
+{
+    if (i == 0) lotto += lottery[i];
+    else lotto += "-" + lottery[i];
+}*/
